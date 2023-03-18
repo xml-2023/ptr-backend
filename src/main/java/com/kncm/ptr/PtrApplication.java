46b507c6813e4df457b9e9e254c0625c00630b49
Marko Savić <savic.ra33.2019@uns.ac.rs>
2023-03-18 14:20:20 +0100

    Initialize project

---
+++ src/main/java/com/kncm/ptr/PtrApplication.java
@@ -0,0 +1,13 @@
+package com.kncm.ptr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kncm/ptr/PtrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kncm.ptr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
